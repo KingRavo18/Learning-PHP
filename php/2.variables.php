58a290fe11ac7505ecb6f109a313ef6fa40c6013
KingRavo18 <rkingovskis14@gmail.com>
2025-09-15 13:26:59 +0300

    g

--- php/2.variables.php
+++ php/2.variables.php
@@ -1,3 +1,48 @@
 <?php
-    // variable = 
+    // variable = a reusable container that holds data
+    //            string, integer, float, boolean
+
+    //STRING
+
+    $name = "Spongebob Squarepants";
+    $food = "pizza";
+    $email = "spinge@gmail.com";
+
+    echo "Hello {$name} <br>";
+    echo "You like {$food} <br>";
+    echo "Your email is {$email} <br>";
+
+    //INTEGER
+
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    echo "You are {$age} years old <br>";
+    echo "There are {$users} users online <br>";
+    echo "You would like to buy {$quantity} items <br>";
+
+    //FLOAT
+
+    $gpa = 2.5;
+    $price = 4.99;
+    $taxRate = 5.1;
+
+    echo "Your gpa is {$gpa} <br>";
+    echo "Your pizza is \${$price} <br>";
+    echo "The sales tax rate is: {$taxRate}% <br>";
+
+    //BOOLEAN
+
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+
+    echo "Online status: {$online} <br><br>";
+
+
+    $total = null;
+    echo "You have ordered {$quantity}x {$food}s <br>";
+    $total = $quantity * $price;
+    echo "Your total is: \${$total}";
 ?>
