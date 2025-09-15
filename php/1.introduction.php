1d29b85e9a8e7c7f1c849ef4b8825d37060dfc97
KingRavo18 <rkingovskis14@gmail.com>
2025-09-15 09:24:54 +0300

    s

---
+++ php/1.introduction.php
@@ -0,0 +1,19 @@
+<?php
+// PHP = PHP Hypertext Preprocessor
+    echo "Hello World <br>";
+    echo "It belongs to me";
+/* This is a 
+multiline comment */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IntroductionToPhp</title>
+</head>
+<body>
+    <br>
+    <button>Order A Pizza</button>
+</body>
+</html>
