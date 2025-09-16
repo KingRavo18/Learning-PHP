d9c38856e9c8c74c20356df63958f4de33bf3f07
KingRavo18 <rkingovskis14@gmail.com>
2025-09-16 13:19:09 +0300

    d

---
+++ php/3.arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+    // Arithmetic operators
+    // + - * / ** %
+
+    $x = 10;
+    $y = 3;
+    $z = null;
+
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    //$z = $x % $y;
+    //echo $z;
+
+    // Increment/decrement operators
+    // ++ --
+
+    $counter = 0;
+    $counter += 2;
+
+    //echo $counter;
+
+    // Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    echo $total;
