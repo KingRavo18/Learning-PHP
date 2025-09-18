9b17e02fa6ed77b3f2310c3ebd6d2bc3550c7700
KingRavo18 <rkingovskis14@gmail.com>
2025-09-18 16:43:23 +0300

    s

--- php/7.logicalOperators.php
+++ php/7.logicalOperators.php
@@ -1,2 +1,62 @@
 <?php
-    // Logical operators
+    // Logical operators = combine conditional statements
+    // if(condition1 && condition2)
+
+    // && = True if bother conditions are true
+    // || = True if at least one condition is true
+    // ! = True if false. False if true.
+
+    $temp = 25;
+    $cloudy = true;
+
+    /*
+    if($temp >= 0 && $temp <= 30){
+        echo "The weather is ok.";
+    }
+    else{
+        echo "The weather is bad.";
+    }
+    */
+
+    if($temp < 0 || $temp > 30){
+        echo "The weather is bad.";
+    }
+    else{
+        echo "The weather is ok.";
+    }
+
+    echo "<br>"; 
+
+    if(!$cloudy){
+        echo "It's sunny";
+    }
+    else{
+        echo "It's cloudy";
+    }
+
+    echo "<br>";
+
+    $age = 20;
+    $citizen = true;
+
+    if(!$age >= 18 || !$citizen){
+        echo "You cannot vote";
+    }
+    else{
+        echo "You can vote";
+    }
+
+    echo "<br>";
+
+    $child = true;
+    $senior = false;
+    $ticket = null;
+
+    if($child || $senior){
+        $ticket = 10;
+    }
+    else{
+        $ticket = 15;
+    }
+
+    echo "The ticket price is \${$ticket}";
