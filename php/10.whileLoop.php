38fa334cc927977d09e0804be849c690d9484d38
KingRavo18 <rkingovskis14@gmail.com>
2025-09-19 08:35:09 +0300

    s

--- php/10.whileLoop.php
+++ php/10.whileLoop.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="10.whileLoop.php" method="POST">
+        <input type="submit" name="stop" value="stop">
+    </form>
+</body>
+</html>
 <?php
 
-    // while loop
+    // while loop = do some code infinetely while some 
+    //              condition remains true
+
+    $counter = 0;
+    while($counter <= 10){
+        echo $counter . "<br>";
+        $counter++;
+    }
+
+    echo "<br>";
+
+    $seconds = 0;
+    $running = true;
+
+    while($running){
+
+        if(isset($_POST["stop"])){
+            $running = false;
+        }
+        else{
+            //wait 1 second
+            $seconds++;
+            echo $seconds . "<br>";
+        }
+    }
+
+?>
