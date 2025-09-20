21718cd79c6e5c1f54a051e2d1650925c025f6d0
KingRavo18 <rkingovskis14@gmail.com>
2025-09-20 18:21:51 +0300

    h

--- php/14.radioButtons.php
+++ php/14.radioButtons.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="14.radioButtons.php" method="POST">
+        <input type="radio" name="credit_card" value="Visa">
+        Visa<br>
+        <input type="radio" name="credit_card" value="Mastercard">
+        Mastercard<br>
+        <input type="radio" name="credit_card" value="American Express">
+        American Express<br>
+        <br>
+        <input type="submit" name="confirm" value="Confirm">
+    </form>
 </body>
 </html>
+<?php
+    if(empty($_POST["confirm"])) die;
+    if(empty($_POST["credit_card"])) die("Please select a payment method");
+    $credit_card = $_POST["credit_card"];
+    echo "You selected {$credit_card}";
+?>
