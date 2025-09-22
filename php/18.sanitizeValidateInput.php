03fda891c8f58b329caf7583c7c30ed31bad323e
KingRavo18 <rkingovskis14@gmail.com>
2025-09-22 12:29:27 +0300

    w

--- php/18.sanitizeValidateInput.php
+++ php/18.sanitizeValidateInput.php
@@ -6,9 +6,49 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="18.sanitizeValidateInput.php" method="POST">
+        <label>
+            username:
+            <input type="text" name="username">
+        </label>
+        <br>
+        <label>
+            age:
+            <input type="text" name="age">
+        </label>
+        <br>
+        <label>
+            email:
+            <input type="text" name="email">
+        </label>
+        <br>
+        <input type="submit" name="login" value="login">
+    </form>
 </body>
 </html>
 <?php
+    if(isset($_POST["login"])){
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        //$age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+        //$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 
+        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+
+        if(empty($age)){
+            echo "That number wasn't valid";
+        }
+        else{
+            echo $age;
+        }
+
+        echo "<br>";
+
+        if(empty($email)){
+            echo "That email wasn't valid";
+        }
+        else{
+            echo $email;
+        }
+    }
 ?>
