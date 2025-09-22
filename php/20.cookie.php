fb78f1653f382ca73c82b0bc972f6573aa0c492d
KingRavo18 <rkingovskis14@gmail.com>
2025-09-23 01:22:28 +0300

    d

--- php/20.cookie.php
+++ php/20.cookie.php
@@ -1,2 +1,19 @@
 <?php
-    // cookie = 
+    // cookie = Information about a user stored in a user's web-browser
+    //          targeted advertisements, browsing preferences, and other
+    //          non-sensitive data
+
+    setcookie("favoriteFood", "pizza", time() - 0, "/");
+    setcookie("favoriteDrink", "water", time() + (86400 * 4), "/");
+    setcookie("favoriteDessert", "ice cream", time() + (86400 * 3), "/");
+
+    foreach($_COOKIE as $key => $value){
+        echo "{$key} => {$value} <br>";
+    }
+
+    if(isset($_COOKIE["favoriteDrink"])){
+        echo "You like {$_COOKIE["favoriteDrink"]}.";
+    }
+    else{
+        echo "I don't know your preferences";
+    }
