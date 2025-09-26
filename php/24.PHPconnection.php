daeabc27deac5690293b83295107ffd38fc922c8
KingRavo18 <rkingovskis14@gmail.com>
2025-09-27 01:42:52 +0300

    h

--- php/24.PHPconnection.php
+++ php/24.PHPconnection.php
@@ -1,2 +1,17 @@
 <?php
-    // 1. MySQLi
+    // 1. MySQLi Extension
+    // 2. PDO (PHP Data Objects)
+
+    include("24.database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br> Hello <br>
+</body>
+</html>
